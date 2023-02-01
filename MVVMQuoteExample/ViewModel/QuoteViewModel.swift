20ce1a63bdb499a287c53f494b83b953f115e096
Paulo Henrique <phsilva098@gmail.com>
2023-02-01 15:12:44 -0300

    feat: criado quote viewmodel

---
+++ MVVMQuoteExample/ViewModel/QuoteViewModel.swift
@@ -0,0 +1,24 @@
+//
+//  QuoteViewModel.swift
+//  MVVMQuoteExample
+//
+//  Created by Paulo Henrique Gomes da Silva on 01/02/23.
+//
+
+import Foundation
+
+class QuoteViewModel {
+    var bind: ((_ model: Quote) -> Void)?
+    var quote: Quote = Quote(anime: "", character: "", quote: "")
+
+    func getQuote() {
+        Task {
+            do {
+                self.quote = try await API().fetch()
+                bind?(self.quote)
+            } catch {
+                print("Invalid request: \(error)")
+            }
+        }
+    }
+}
