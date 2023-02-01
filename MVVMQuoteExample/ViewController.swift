b3900551e0aad577b2e65c2c878fb8e223ada1d7
Paulo Henrique <phsilva098@gmail.com>
2023-02-01 14:24:02 -0300

    Enviando arquivos

---
+++ MVVMQuoteExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMQuoteExample
+//
+//  Created by Paulo Henrique Gomes da Silva on 01/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
